dbc6eb7f6cab0bb3d68d7e57b9181bf4adc9f935
CasparBosch <1004288@hr.nl>
2023-04-02 23:43:21 +0200

    Funny

--- predict.js
+++ predict.js
@@ -6,16 +6,16 @@ let result = document.getElementById("result")
 
 function modelLoaded() {
     predictButton.addEventListener("click", () => {
-        let sale = parseInt(document.getElementById("sale").value)
+        let displacement = parseInt(document.getElementById("displacement").value)
         let weight = parseInt(document.getElementById("weight").value)
-        let resolution = parseInt(document.getElementById("resolution").value)
-        makePrediction(sale, weight, resolution)
+        let acceleration = parseInt(document.getElementById("acceleration").value)
+        makePrediction(displacement, weight, acceleration)
     
 
     })
 }
 
-async function makePrediction(sale, weight, resolution) {
-    const pred = await nn.predict({ sale: sale, weight: weight, resolution: resolution})
+async function makePrediction(displacement, weight, acceleration) {
+    const pred = await nn.predict({ displacement: displacement, weight: weight, acceleration: acceleration})
     result.innerHTML = `Price is ${pred[0].price}`
 }
--- train.js
+++ train.js
@@ -1,5 +1,7 @@
 import { createChart, updateChart } from "./scatterplot.js";
 const nn = ml5.neuralNetwork({ task: "regression", debug: true });
+let button = document.getElementById("btn");
+let result = document.getElementById("result");
 
 function loadData() {
     Papa.parse("./data/cars.csv", {
@@ -14,31 +16,46 @@ function checkData(data) {
     // shuffle
     data.sort(() => Math.random() - 0.5);
 
+    // split in train en test data
+    let trainData = data.slice(0, Math.floor(data.length * 0.8))
+    let testData = data.slice(Math.floor(data.length * 0.8) + 1)
+
     // kijk hoe de data eruit ziet
     console.table(data);
 
     // een voor een de data toevoegen aan het neural network
-    for (let car of data) {
-        nn.addData({ horsepower: car.horsepower }, { mpg: car.mpg });
+    for (let car of trainData) {
+        nn.addData({ horsepower: car.horsepower, displacement: car.displacement, weight: car.weight, acceleration: car.acceleration}, { mpg: car.mpg });
     }
 
     // normalize
     nn.normalizeData();
 
-    // x = horsepower, y = mpg
+    // x = horsepower, displacement, weight, acceleration, y = mpg
     const chartdata = data.map((car) => ({
         x: car.horsepower,
+        x: car.displacement,
+        x: car.weight,
+        x: car.acceleration,
         y: car.mpg,
     }));
 
     // kijk hoe de data eruit ziet
-    console.log(chartdata);
+    // console.log(chartdata);
 
     // chartjs aanmaken
     createChart(chartdata, "Horsepower", "MPG");
 
     // start training
     startTraining();
+
+    button.addEventListener("click", () => {
+        let horsepower = parseInt(document.getElementById("horsepower").value);
+        let displacement = parseInt(document.getElementById("displacement").value);
+        let weight = parseInt(document.getElementById("weight").value);
+        let acceleration = parseInt(document.getElementById("acceleration").value);
+        makePrediction(horsepower, displacement, weight, acceleration);
+    });
 
     // voorspelling maken
     makePrediction();
@@ -47,7 +64,7 @@ function checkData(data) {
     updateChart();
 }
 
-loadData();
+// loadData();
 
 function startTraining() {
     nn.train({ epochs: 10 }, () => finishedTraining());
@@ -60,12 +77,19 @@ async function finishedTraining() {
         predictions.push({x: hp, y: pred[0].mpg})
     }
     updateChart("Predictions", predictions)
+
+    nn.save()
 }
 
-async function makePrediction() {
-    const results = await nn.predict({ horsepower: 90 });
-    console.log(`Geschat verbruik: ${results[0].mpg}`);
+async function makePrediction(displacement, weight, acceleration) {
+    const results = await nn.predict({ horsepower: horsepower, displacement: displacement , weight: weight, acceleration: acceleration });
+    result.innerHTML = `Geschat verbruik: ${results[0].price}`;
 }
+
+// async function makePrediction() {
+//     const results = await nn.predict({ horsepower: 90 });
+//     console.log(`Geschat verbruik: ${results[0].mpg}`);
+// }
 
 //
 // demo data
@@ -79,3 +103,5 @@ async function makePrediction() {
 //         { horsepower: 193, mpg: 9 },
 //         { horsepower: 80, mpg: 25 },
 // ]
+
+loadData();
